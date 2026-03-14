b2c4122ccbb401de3676d0ca262efaf7c689de22
imsecret15 <newbloxy2022@gmail.com>
2026-03-15 00:06:36 +0100

    Create MM_Menu.lua

---
+++ MM_Menu.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/imsecret15/roblox_system1/main/MmScript.lua"))()
+
+local Window = Library.CreateLib("Mortem Metallum [Alpha]", "DarkTheme")
+
+local Tab = Window:NewTab("Main")
+
+local Section = Tab:NewSection("Step 1")
+
+Section:NewButton("Dupe Crossbows", "", function()
+print("dupe code here")
+end)
